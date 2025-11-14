5d80dbca65cb533d082bb2c02c87d5a98edb7064
NathanJFeldman <nathanfeldman.init@gmail.com>
2025-11-14 10:18:00 +0100

    Exception error tests and save function to main code

--- src/main/java/se/nackademin/devops24/pingurl/repository/MemoryURLRepository.java
+++ src/main/java/se/nackademin/devops24/pingurl/repository/MemoryURLRepository.java
@@ -13,11 +13,15 @@ public class MemoryURLRepository implements URLRepository {
 
     @Override
     public void save(String name, String url) {
+        if (urls.containsKey(name)) {
+            throw new IllegalArgumentException(name + "already exists!");
+        }
         PingedURL pingedURL = new PingedURL()
                 .setName(name)
                 .setUrl(url)
                 .setCreatedAt(LocalDateTime.now());
-        urls.put(name, pingedURL);
+        urls.put(name, pingedURL) ;
+
     }
 
     @Override
