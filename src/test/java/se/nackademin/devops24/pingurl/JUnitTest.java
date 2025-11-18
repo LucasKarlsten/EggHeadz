ed8a3c5e011f4b94d31979dc18a5fd454213ab46
NathanJFeldman <nathanfeldman.init@gmail.com>
2025-11-18 15:07:27 +0100

    Remove duplicate in JUnitTest

--- src/test/java/se/nackademin/devops24/pingurl/JUnitTest.java
+++ src/test/java/se/nackademin/devops24/pingurl/JUnitTest.java
@@ -67,6 +67,8 @@ public class JUnitTest {
         page.click("table tr:nth-child(2) form button[type='submit']");
         page.click("tr:has(td:has-text('Nackademin.se')) form:has(button:has-text('Delete')) button");
         page.click("tr:has(td:has-text('DN.se')) form:has(button:has-text('Delete')) button");
+
+
     }
 
 }
