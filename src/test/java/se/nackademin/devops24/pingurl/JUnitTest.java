1065636b1b248f552b0504dc4595a0e6fd871dcb
NathanJFeldman <nathanfeldman.init@gmail.com>
2025-11-18 12:03:17 +0100

    Automatiserat test i JUnit med hjälp av playwright

---
+++ src/test/java/se/nackademin/devops24/pingurl/JUnitTest.java
@@ -0,0 +1,53 @@
+package se.nackademin.devops24.pingurl;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class JUnitTest {
+    private Playwright playwright;
+    private Browser browser;
+    private BrowserContext context;
+    private Page page;
+
+    @BeforeAll
+    void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext(new Browser.NewContextOptions().setIgnoreHTTPSErrors(true));
+    }
+
+    @BeforeEach
+    void createPage() {
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closePage() {
+        if (page != null) {
+            page.close();
+        }
+    }
+
+    @AfterAll
+    void afterTest() {
+        if (context != null) context.close();
+        if (browser != null) browser.close();
+        if (playwright != null) playwright.close();
+    }
+
+    @Test
+    void testAddUserandPing() {
+        page.navigate("http://localhost:8080");
+        page.fill("input[name='name']:visible", "Google.com");
+        page.fill("input[name='url']:visible", "https://www.google.com");
+        page.click("input[type='submit']");
+        page.fill("input[name='name']:visible", "DN.se");
+        page.fill("input[name='url']:visible", "https://www.DN.se");
+        page.click("input[type='submit']");
+        page.click("table tr:nth-child(1) form button[type='submit']");
+        page.click("table tr:nth-child(2) form button[type='submit']");
+
+    }
+}
